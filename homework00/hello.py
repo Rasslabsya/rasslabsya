f5752f48a77ecbd73579c94b9996f0fee97efaf0
Rasslabsya <113046565+Rasslabsya@users.noreply.github.com>
2022-09-21 13:22:01 +0300

    Update hello.py

--- homework00/hello.py
+++ homework00/hello.py
@@ -1,6 +1,6 @@
 def get_greeting(name: str) -> str:
     return "hello, " + name  + "!"
 
-
-
-print(get_greeting('vasea'))
+if __name__ == "__main__":
+    message = get_greeting('DANYA')
+    print(message)
